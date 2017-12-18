f6687c9ba875592313701db2407d83f77b212190
bottaid <dariobottai@gmail.com>
2017-12-18 19:50:00 +0100

    Documents Overview

---
+++ src/it/bottai/Main.java
@@ -0,0 +1,41 @@
+package it.bottai;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("GykoPdfConverter");
+
+        initRootLayout();
+    }
+
+    public void initRootLayout() {
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
