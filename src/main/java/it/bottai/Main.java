00f613401e70439c82be01efa5f8cda1b67c9b46
Bromans <michele.romani.gzl0@me.com>
2018-01-19 10:41:25 +0100

    Conversion of the JavaFX project into a JavaFX Maven project

--- src/main/java/it/bottai/Main.java
+++ src/main/java/it/bottai/Main.java
@@ -1,28 +1,13 @@
 package it.bottai;
 
-import it.bottai.model.Esercizio;
 import it.bottai.view.DocumentsOverviewController;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.*;
-import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import java.awt.Desktop;
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class Main extends Application {
     private Stage primaryStage;
@@ -51,7 +36,7 @@ public class Main extends Application {
     public void initRootLayout() {
         try{
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+            loader.setLocation(Main.class.getResource("/views/RootLayout.fxml"));
             rootLayout = (BorderPane) loader.load();
 
             Scene scene = new Scene(rootLayout);
@@ -67,7 +52,7 @@ public class Main extends Application {
         try {
             // Carico l'interfaccia dei documenti
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("view/DocumentsOverview.fxml"));
+            loader.setLocation(Main.class.getResource("/views/DocumentsOverview.fxml"));
             AnchorPane documentsOverview = (AnchorPane) loader.load();
 
             // Posiziona al centro l'interfaccia
--- src/main/java/it/bottai/model/Esercizio.java
+++ src/main/java/it/bottai/model/Esercizio.java
@@ -6,9 +6,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public interface Esercizio {
-    public void estraiRigheTabella(ArrayList<String> lista);
+    public ArrayList<String> estraiRigheTabella(ArrayList<String> lista);
     public void scriviRigheTabella(ArrayList<String> righeTabella, String outputDir, String filename) throws IOException;
-    public void parsaRigheTabella(ArrayList<String> righeTabella);
+    public ArrayList<String> parsaRigheTabella(ArrayList<String> righeTabella);
     public StringProperty primaColonnaProperty();
     public StringProperty secondaColonnaProperty();
 }
